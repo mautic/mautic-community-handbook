c712a50bd8a5410e698366ba7247a86592b6e394
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-10-25 18:48:22 +0100

    Initial commit to set up Read the Docs support

---
+++ docs/about/what_is_mautic.rst
@@ -0,0 +1,18 @@
+What is Mautic?
+###############
+
+Mautic is the world's first Open Source Marketing Automation platform.
+
+It was created by DB Hurley and launched in 2014 as an Open Source project under the GPL v3 license. You can find out more on our website,
+:xref:`mautic.org`, or on the :xref:`Mautic GitHub repository`. You can :xref:`download Mautic` for free on mautic.org.
+
+The Mautic Community are a worldwide distributed team of volunteers who are passionate about Mautic, Marketing Automation, and Open Source technologies.
+
+We come together to build, improve and share Mautic, united by our belief in empowering organizations with the tools they need to deliver world-class automation and personalized experiences to their customers.
+
+Anybody can get involved - whether you want to organise a local meetup, contribute code, improve our documentation or translations, or help us tell others about Mautic.
+
+We are primarlily organized around the following channels: 
+
+:xref:`Mautic Community Forums`
+:xref:`Mautic Community Slack`
---
+++ docs/conf.py
@@ -0,0 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+
+import sys, os
+import sphinx_rtd_theme
+
+sys.path.append(os.path.abspath('ext'))
+sys.path.append('.')
+
+from links.link import *
+from links import *
+
+# -- Project information
+
+project = 'Mautic Community Handbook'
+copyright = '2022, Mautic'
+author = 'Mautic'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+source_suffix = ['.rst', '.md']
+
+extensions = [
+    'xref',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
+    'myst_parser'
+]
+
+myst_enable_extensions = [
+    "linkify",
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+html_static_path = ['css']
+
+# -- Options for HTML output
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
