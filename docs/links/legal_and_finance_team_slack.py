8152da20532a6a937853092f0cd0343aaf297f16
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-10-06 13:48:26 -0700

    Add legal and finance team

---
+++ docs/links/legal_and_finance_team_slack.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Legal and Finance Team" 
+link_text = "#t-legal-and-finance" 
+link_url = "https://mautic.slack.com/archives/CQGQ1H2SE" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
