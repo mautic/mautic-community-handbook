05d16ddf44997a0b52c07646c89480395d82802c
Olawore Hikmah <105384156+Iqmaa@users.noreply.github.com>
2024-10-07 14:48:45 +0100

    Update Jono-bacon-video.py
    correcting error prevet-prevent

--- docs/links/Jono-bacon-video.py
+++ docs/links/Jono-bacon-video.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "Jono Bacon's how to prevet burnout video"
-link_text = "Jono Bacon's how to prevet burnout video"
+link_name = "Jono Bacon's how to prevent burnout video"
+link_text = "Jono Bacon's how to prevent burnout video"
 link_url = "https://www.youtube.com/watch?v=WC1bpYGoB7s" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
