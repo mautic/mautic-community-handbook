d9ef8b9c7e567b06e7f237494ac9d7bb32ffcdfb
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-10-22 02:38:44 +0100

    Update the link to the GSOC projects

--- docs/links/GSOC-ideas.py
+++ docs/links/GSOC-ideas.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "GSOC project ideas"
 link_text = "GSOC project ideas"
-link_url = "https://docs.google.com/document/d/1iqjmKcIelXCnDcfdD7ZxSE6H1VV1gMOKnnft7gD30Dk/edit?tab=t.0#heading=h.eigxwt8usaos" 
+link_url = "https://community.mautic.org/assemblies/gsoc/f/93/" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
