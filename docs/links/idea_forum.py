6cd6a0ad2c768c5ac6eac72576926d0b0a5f21e6
Rita Onwudiwe <onwudiwechinwendu16@gmail.com>
2024-10-31 22:10:54 +0000

    conversion for RFP and add-ons

---
+++ docs/links/idea_forum.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Ideas and Feature Requests" 
+link_text = "Ideas and Feature Requests"
+link_url = "https://forum.mautic.org/ideas" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
---
+++ docs/links/obs_model.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "OBS open-source project’s model" 
+link_text = "OBS open-source project’s model"
+link_url = "https://github.com/obsproject/obs-studio/wiki/OBS-Project-Bounty-Program" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
