5b9529a04b83c93ace2953a8878386bd55cacaf0
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-11-04 22:45:30 +0000

    Add GH Search exclusion

--- docs/conf.py
+++ docs/conf.py
@@ -69,4 +69,6 @@ locale_dirs = ['locale']
 linkcheck_ignore = [
       # Anchors are picked up as broken
     r"https://docs.mautic.org/policies/financial-policy#10-foreign-assets-control",
+    # The GitHub Search UI requires users to be authenticated with session cookies, which we can't set up programmatically
+    r"https://github.com/search*",
 ]
