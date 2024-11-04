76f23beee386121ed7b5b319e1f7df019726237b
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-11-04 06:00:11 +0000

    Fix broken link

--- docs/links/idea_forum.py
+++ docs/links/idea_forum.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Ideas and Feature Requests" 
 link_text = "Ideas and Feature Requests"
-link_url = "https://forum.mautic.org/ideas" 
+link_url = "https://forum.mautic.org/c/ideas" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
