4c1b5319f298ef5cd6ffcd78109876e36af4f2e2
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-11-04 06:05:09 +0000

    Fix broken links causing build to fail

--- docs/links/Mautic_pull_request_requirements.py
+++ docs/links/Mautic_pull_request_requirements.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Mautic-pr-requirements" 
 link_text = "Mautic-pr-requirements"
-link_url = "https://docs.mautic.org/contributing-to-mautic/developer/code/pull-requests" 
+link_url = "https://contribute.mautic.org/contributing-to-mautic/developer/code/pull-requests" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
