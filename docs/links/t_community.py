3388e5e2bf83ab1c8a5a95f6b3fe1228ffbf5882
Patrick Jenkner <p.jenkner@leuchtfeuer.com>
2024-11-05 12:18:25 +0000

    add page for community team; rename sections in L&f for consitency

---
+++ docs/links/t_community.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "t-community" 
+link_text = "t-community" 
+link_url = "https://mautic.slack.com/archives/CQV40ULMA" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
