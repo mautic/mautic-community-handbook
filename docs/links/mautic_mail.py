904fbb53ac90a6ea872eb34fa0c00dc722cf819a
Rita Onwudiwe <onwudiwechinwendu16@gmail.com>
2024-11-19 20:45:29 +0100

    Added the mailto
    Co-authored-by: Ruth Cheesley <ruth@ruthcheesley.co.uk>

--- docs/links/mautic_mail.py
+++ docs/links/mautic_mail.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Mautic Mail" 
 link_text = "Mautic Mail" 
-link_url = "security@mautic.org" 
+link_url = "mailto:security@mautic.org" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
