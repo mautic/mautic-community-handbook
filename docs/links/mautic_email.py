f0f2a2d34f25e79d9d57a293a91c2dd5cff120c0
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-02-20 11:38:14 +0000

    Fix typo in mailto: breaking the link

--- docs/links/mautic_email.py
+++ docs/links/mautic_email.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "info@mautic.org" 
 link_text = "info@mautic.org"
-link_url = "maito:info@mautic.org" 
+link_url = "mailto:info@mautic.org" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
