d513c8bd072f2fecbe6e43eb3a9b82321e6285ec
Nickyshe <judypearls200@gmail.com>
2025-04-02 03:02:04 +0100

    Update GitHub Links

--- docs/links/github_pr_8986.py
+++ docs/links/github_pr_8986.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "github_pr_8986" 
-link_text = "github_pr_8986"
+link_text = "GitHub PR #8986"
 link_url = "https://github.com/mautic/mautic/issues/8986" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
--- docs/links/mautic_label.py
+++ docs/links/mautic_label.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "quite a lot of labels" 
 link_text = "quite a lot of labels"
-link_url = "https://github.com/mautic/mautic/labels?page=1&sort=name-asc" 
+link_url = "https://github.com/mautic/mautic/labels" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
