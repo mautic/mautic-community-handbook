55cf740126880b0a2869df589a196e74250323e1
Judith Etugbo <judypearls200@gmail.com>
2025-04-02 02:23:43 +0100

    Update docs/links/github_pr_6875.py
    Co-authored-by: Ruth Cheesley <ruth@ruthcheesley.co.uk>

--- docs/links/github_pr_6875.py
+++ docs/links/github_pr_6875.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "github_pr_6875" 
-link_text = "github_pr_6875"
+link_text = "GitHub PR #6875"
 link_url = "https://github.com/mautic/mautic/pull/6875" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
