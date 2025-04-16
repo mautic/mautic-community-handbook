f8c9710cd147dc8307040b0255bddcdf5560b491
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-16 15:03:54 +0000

    feat: add mautic_troubleshooting.py to the links folder

---
+++ docs/links/mautic_troubleshooting.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic troubleshooting" 
+link_text = "Troubleshooting" 
+link_url = "https://community.mautic.org" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
