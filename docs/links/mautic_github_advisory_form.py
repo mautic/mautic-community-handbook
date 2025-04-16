1eed7856458cd86a6c74a667b1f0d2d23b00f501
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-16 14:19:57 +0000

    feat: add mautic_github_advisory_form.py

---
+++ docs/links/mautic_github_advisory_form.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic GitHub advisory form" 
+link_text = "Advisory form" 
+link_url = "https://github.com/mautic/mautic/security/advisories/new" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
