90e2508b2aed174c7cd7c2233620c5e95dd13893
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-29 21:02:25 +0000

    feat: add docs/links/rst_site.py

---
+++ docs/links/rst_site.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "reStructuredText" 
+link_text = "reStructuredText" 
+link_url = "https://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
