74209c551a8c89856c271b6eb82bfd4ecfa33aea
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-29 18:56:56 +0000

    fix: remove github_cli_install.py

--- docs/links/github_cli_install.py
+++
@@ -1,7 +0,0 @@
-from . import link
-
-link_name = "install GitHub command-line tool" 
-link_text = "install GitHub CLI" 
-link_url = "https://cli.github.com/" 
-
-link.xref_links.update({link_name: (link_text, link_url)})
