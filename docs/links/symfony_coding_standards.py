c7425a00ea0dc4b72abd415d087ab97655896ba9
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-29 18:54:13 +0000

    feat: add docs/links/symfony_coding_standards.py

---
+++ docs/links/symfony_coding_standards.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Symfony coding standards"
+link_text = "Coding Standard"
+link_url = "https://symfony.com/doc/current/contributing/code/standards.html"
+
+link.xref_links.update({link_name: (link_text, link_url)})
