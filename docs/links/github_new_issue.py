74620247c1f782553d3685328b3b1e46759ca5c1
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-29 21:27:46 +0000

    fix: change link_text and link_name for reusing purpose

--- docs/links/github_new_issue.py
+++ docs/links/github_new_issue.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "GitHub create an issue" 
-link_text = "a new issue on GitHub" 
+link_name = "Create a new issue for Mautic" 
+link_text = "issue" 
 link_url = "https://github.com/mautic/mautic/issues/new?assignees=&labels=bug%2Cneeds-triage&template=BUG-REPORT.yml&title=Your+bug+title+goes+here%21" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
