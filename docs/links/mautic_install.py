50415cdf916fb079ed19b9d2bd6de91caa5f127e
Ayu Adiati <retno.ayu98@gmail.com>
2025-04-29 18:58:23 +0000

    fix: change link_text in docs/links/mautic_install.py for reuse purpose

--- docs/links/mautic_install.py
+++ docs/links/mautic_install.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "install Mautic"
-link_text = "install Mautic"
+link_text = "Mautic"
 link_url = "https://docs.mautic.org/en/setup/how-to-install-mautic"
 
 link.xref_links.update({link_name: (link_text, link_url)})
