bd65930f8b9f81dd53c6cc3de82fa6f51f681df5
Ayu Adiati <retno.ayu98@gmail.com>
2025-06-17 19:18:51 +0000

    remove duplicate link

--- docs/links/mautic_partners.py
+++
@@ -1,7 +0,0 @@
-from . import link
-
-link_name = "Mautic Partners" 
-link_text = "Mautic Partners" 
-link_url = "https://mau.tc/partners" 
-
-link.xref_links.update({link_name: (link_text, link_url)})
