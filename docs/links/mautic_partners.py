35905af60801011ceaabefa9aaf1bf0c7810a345
Ayu Adiati <retno.ayu98@gmail.com>
2025-06-25 08:17:36 +0000

    fix: link text for mautic partners

--- docs/links/mautic_partners.py
+++ docs/links/mautic_partners.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "Mautic Partners" 
-link_text = "Mautic Partners"
+link_text = "Mautic Community Partners"
 link_url = "https://mautic.org/members-partners/" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
