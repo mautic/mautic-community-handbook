f3e1d31471c78ab5b80d1f7818268d7738501696
Judith Etugbo <judypearls200@gmail.com>
2025-07-01 11:53:59 +0100

    Update docs/links/mautic_label.py
    Co-authored-by: Ayu Adiati <45172775+adiati98@users.noreply.github.com>

--- docs/links/mautic_label.py
+++ docs/links/mautic_label.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "Mautic labels" 
-link_text = "quite a lot of labels"
+link_text = "PR and issue labels"
 link_url = "https://github.com/mautic/mautic/labels" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
