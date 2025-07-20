c91e214284bdf126d4fce2c68d31138477b35283
Ayu Adiati <45172775+adiati98@users.noreply.github.com>
2025-07-20 20:17:48 +0000

    make links

---
+++ docs/links/eslint_year_paying_contributors_review.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "ESLint A year of paying contributors review" 
+link_text = "ESLint" 
+link_url = "https://eslint.org/blog/2020/10/year-paying-contributors-review" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
