13fad4009120f38478d6c81dc6ad51d8d4e49c2b
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-08-01 14:35:09 +0100

    Restore name and link, and use correct naming convention

--- docs/links/mautic_community_portal.py
+++ docs/links/mautic_community_portal.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "community-portal" 
-link_text = "https://community.mautic.org" 
+link_name = "Community Portal" 
+link_text = "Community Portal" 
 link_url = "https://community.mautic.org" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
