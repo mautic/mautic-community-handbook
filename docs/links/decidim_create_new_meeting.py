504abe57853b8ed45cfef208bef1a562ad6aeac3
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-08-01 14:47:13 +0100

    Update Decidim docs links

--- docs/links/decidim_create_new_meeting.py
+++ docs/links/decidim_create_new_meeting.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Decidim create a new meeting" 
 link_text = "Decidim documentation" 
-link_url = "https://docs.decidim.org/en/develop/admin/components/meetings.html#_create_a_new_meeting" 
+link_url = "https://docs.decidim.org/en/develop/admin/components/meetings/meetings" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
--- docs/links/decidim_new_assembly_form.py
+++ docs/links/decidim_new_assembly_form.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Decidim new assembly form" 
 link_text = "Decidim documentation"
-link_url = "https://docs.decidim.org/en/develop/admin/spaces/assemblies#_new_assembly_form" 
+link_url = "https://docs.decidim.org/en/develop/admin/spaces/assemblies/assembly_creation" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
