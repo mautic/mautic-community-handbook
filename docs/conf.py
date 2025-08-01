b29f0bee154a1c6bf95abe59efd6c762a96f06c8
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-08-01 15:04:02 +0100

    Add canva to link check ignore file

--- docs/conf.py
+++ docs/conf.py
@@ -103,4 +103,6 @@ linkcheck_ignore = [
     r"https://www.unsplash.com",
     # Anchor is picked up as not found
     r"https://github.com/mautic/mautic/blob/8a57278758e2c3e1c1ca987aaf9ebd5f05b3c877/app/bundles/CampaignBundle/Executioner/Dispatcher/LegacyEventDispatcher.php#L201",
+    # 403 errors from this domain
+    r"https://www.canva.com/*"
 ]
