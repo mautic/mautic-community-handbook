a5d8e7dcd0407eb48565ae290e5e79824e7b725a
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-08-01 17:46:14 +0100

    Fix incorrect link to supported versions & rename to adhere to convention.

--- docs/links/mautic_supported_versions.py
+++ docs/links/mautic_supported_versions.py
@@ -2,5 +2,5 @@ from . import link
 
 link_name = "Supported Versions"
 link_text = "Supported Versions"
-link_url = "https://github.com/mautic/mautic#supported-versions"
+link_url = "https://mautic.org/releases/"
 link.xref_links.update({link_name: (link_text, link_url)})
