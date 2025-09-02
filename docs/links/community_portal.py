62a6e30b8b273f1ec8e419a823347ca1651d80cb
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-09-02 14:21:58 +0100

    Remove duplicated link

--- docs/links/community_portal.py
+++
@@ -1,7 +0,0 @@
-from . import link
-
-link_name = "Community Portal" 
-link_text = "Community Portal" 
-link_url = "https://community.mautic.org" 
-
-link.xref_links.update({link_name: (link_text, link_url)})
