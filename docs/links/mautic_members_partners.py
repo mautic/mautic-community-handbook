99d41271224d62b298706a41e167f39954da6a2d
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-09-09 13:59:57 +0100

    Update content, improve grammar, remove vale off where not needed.

---
+++ docs/links/mautic_members_partners.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic Members and Partners database" 
+link_text = "Members and Partners database" 
+link_url = "https://mautic.org/members-partners/" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
