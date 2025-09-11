625d67aa36184a31d1f8c843af4392489646c1e9
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2025-09-11 22:00:17 +0100

    Fix the broken link We used to link out to a Sphinx and Sublime documentation resource which was removed, so the link was broken.
    We now have this information in the contribution.md file so I've updated the link accordingly.

--- docs/links/sphinx_xref_role.py
+++ docs/links/sphinx_xref_role.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "xref role documentation" 
 link_text = ":xref: role documentation" 
-link_url = "https://sublime-and-sphinx-guide.readthedocs.io/en/latest/references.html#add-link-make-command" 
+link_url = "https://github.com/mautic/mautic-community-handbook/blob/main/.github/CONTRIBUTING.md#create-a-new-link" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
