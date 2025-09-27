8afc11feeeba17a3efb7ca32fb277afea3214c41
Ayu Adiati <retno.ayu98@gmail.com>
2025-09-27 19:33:04 +0200

    add link for github codespaces

---
+++ docs/links/github_codespaces.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "GitHub Codespaces" 
+link_text = "GitHub Codespaces" 
+link_url = "https://github.com/features/codespaces" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
