9edc9d7053ae3f76d967ca51f4026eb7a94ada3d
Efe Akhigbe <73133486+EfeAkhigbe@users.noreply.github.com>
2025-10-08 07:30:07 +0100

    make links

---
+++ docs/links/mautic_no_code_tasks_board.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic No Code Tasks Board" 
+link_text = "Mautic No Code Tasks Board" 
+link_url = "https://github.com/orgs/mautic/projects/21/views/1" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
