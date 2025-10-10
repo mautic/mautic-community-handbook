be2c40729699c8d044e7b20db57a6edae295fe67
Efe Akhigbe <73133486+EfeAkhigbe@users.noreply.github.com>
2025-10-10 17:13:49 +0100

    Update docs/links/mautic_no_code_tasks_board.py
    Co-authored-by: Ayu Adiati <45172775+adiati98@users.noreply.github.com>

--- docs/links/mautic_no_code_tasks_board.py
+++ docs/links/mautic_no_code_tasks_board.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "Mautic No Code Tasks Board" 
-link_text = "Mautic No Code Tasks Board" 
+link_name = "Mautic low- and no-code projects board" 
+link_text = "Mautic low- and no-code projects board" 
 link_url = "https://github.com/orgs/mautic/projects/21/views/1" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
