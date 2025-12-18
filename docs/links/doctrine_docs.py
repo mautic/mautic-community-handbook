d1f62f957c06aa84929881fb5555633200bffbbc
Ayu Adiati <retno.ayu98@gmail.com>
2025-12-18 12:43:06 +0100

    fix: broken link

--- docs/links/doctrine_docs.py
+++ docs/links/doctrine_docs.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "Doctrine docs" 
-link_text = "Doctrine's documentation" 
-link_url = "https://www.doctrine-project.org/projects/doctrine-migrations-bundle/en/3.4/index.html"
+link_text = "DoctrineMigrationsBundle documentation" 
+link_url = "https://symfony.com/bundles/DoctrineMigrationsBundle/current/index.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
