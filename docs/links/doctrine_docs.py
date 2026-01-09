a6913f8f7b9d086946d8e32b77c8e64227e33159
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-09 19:41:28 +0100

    Revert "fix: broken links"
    This reverts commit 8853ee2827e5003990a80992988564182f75dc45.

--- docs/links/doctrine_docs.py
+++ docs/links/doctrine_docs.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Doctrine docs" 
 link_text = "Doctrine's documentation" 
-link_url = "https://www.doctrine-project.org/projects/doctrine-migrations-bundle/en/4.0/index.html"
+link_url = "https://www.doctrine-project.org/projects/doctrine-migrations-bundle/en/3.4/index.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
