77539594b9ee4ed6ba8d0949f38f1bd6febfc9cb
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-20 09:03:43 +0100

    update link to include pane

--- docs/links/mautic_low_no_code_projects_github.py
+++ docs/links/mautic_low_no_code_projects_github.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Mautic low-no-code projects board" 
 link_text = "Mautic's No/Low/Code Tasks" 
-link_url = "https://github.com/orgs/mautic/projects/21/views/1" 
+link_url = "https://github.com/orgs/mautic/projects/21/views/1?pane=info" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
